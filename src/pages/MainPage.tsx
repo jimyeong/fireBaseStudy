b12e66b65ba1cfacebdc28012de3152683699a7d
jimyeong <idjjm92@gmail.com>
2019-06-09 01:31:02 +0900

    test

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 const MainPage: React.FunctionComponent = props => {
   const {} = props;
-  return <div className="main page">this is main page</div>;
+  return <div className="main page">this is main page asa</div>;
 };
 
 export default MainPage;
